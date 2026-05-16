af5330cbeb76d0d04da1385b13af23750d71db88
Arshdeep singh <badhanarsh2008@gmail.com>
2026-05-16 23:04:46 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import eventlet
+eventlet.monkey_patch()
 from flask import Flask,render_template,request,session,redirect,url_for
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random 
